2466d8fd18282854688d1ff4b3a2cbb6ddc88ce6
TashiDolma <124641819+TashiDolma@users.noreply.github.com>
2023-02-06 21:59:06 -0500

    Add files via upload

---
+++ ForLoopExample.java
@@ -0,0 +1,7 @@
+public class ForLoopExample {
+    public static void main(String args[]){
+        for(int x=1; x<5; x++){
+            System.out.println("For loop printing value of x = "+x);
+        }
+    }
+}
