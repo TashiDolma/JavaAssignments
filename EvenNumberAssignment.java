e9328c24d782108d919dcfabf8f6c3dd3b615223
TashiDolma <124641819+TashiDolma@users.noreply.github.com>
2023-02-07 16:27:15 -0500

    Add files via upload

---
+++ EvenNumberAssignment.java
@@ -0,0 +1,8 @@
+public class EvenNumberAssignment {
+    public static void main(String args[]) {
+        for (int x = 3; x <= 30; x++) {
+            if (x % 2 == 0) {
+                System.out.println("Even numbers 1value of x = " + x);
+            }
+        }
+    }}
---
+++ OddNumberAssignment.java
@@ -0,0 +1,12 @@
+public class OddNumberAssignment {
+
+    public static void main(String args[]){
+        int X=2;
+
+        while(X<=20){
+            if(X%2 != 0)
+            System.out.println("Odd Numbers  = " +X);
+            X=X+1;
+        }
+    }
+}
