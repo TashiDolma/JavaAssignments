38ebd525176be20257cb6bdaa057100b5390b6dc
TashiDolma <124641819+TashiDolma@users.noreply.github.com>
2023-02-06 21:46:45 -0500

    Add files via upload

---
+++ LoopExample.java
@@ -0,0 +1,11 @@
+public class LoopExample {
+
+    public static void main(String args[]){
+        int X=10;
+
+        while(X<=1000){
+            System.out.println("Print the value of X = " +X);
+            X=X+2;
+        }
+    }
+}
